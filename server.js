38a4f72c46ab7086c127801eec231d06e566edf6
Chirag Sanghvi <csanghvi@appacitive.com>
2017-04-08 14:33:31 -0700

    made mobile compatible

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/', (req, res) => {
  	});
 });
 
-app.listen(process.env.PORT || 3000, function() {
-	console.log('listening on 3000')
+app.listen(process.env.PORT || 5000, function() {
+	console.log('listening on ' + (process.env.PORT || 5000));
 })
 
