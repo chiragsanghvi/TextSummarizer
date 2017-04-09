1ba8dd314d877984d8dc9681da9c6246dad8c60d
Hirak Modi <hirak2525@gmail.com>
2017-04-08 23:32:33 -0700

    Implemented TextRank similarity, add python gitignore

---
+++ summarizer/text_rank_similarity.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from preprocess import cleanText
+import networkx
+import itertools
+import math
+import json
+
+sentenceDictionary = {};
+
+def getSimilarity(sentenceID1, sentenceID2):
+    commonWordCount = len(set(sentenceDictionary[sentenceID1]) & set(sentenceDictionary[sentenceID2]))
+    print str(sentenceID1) + "-"+ str(len(sentenceDictionary[sentenceID1])) + "~~~" + str(sentenceID2) + "-" +str(len(sentenceDictionary[sentenceID2]))
+    denominator = math.log(len(sentenceDictionary[sentenceID1])) + math.log(len(sentenceDictionary[sentenceID2]))
+    return commonWordCount/denominator if denominator else 0
+
+def generateGraph(nodeList):
+    graph = networkx.Graph()
+    graph.add_nodes_from(nodeList)
+    edgeList = list(itertools.product(nodeList, repeat=2))
+    for edge in edgeList:
+        graph.add_edge(edge[0], edge[1], weight=getSimilarity(edge[0], edge[1]))
+    return graph
+
+
+def printDictionary():
+    for key,val in sentenceDictionary.iteritems():
+        print str(key) + " : " + " ".join(sentenceDictionary[key])
+
+def textRankSimilarity(summarySentenceCount):
+    fileName = "../Marathi/documents/doc1"
+    global sentenceDictionary
+    sentenceDictionary, size = cleanText(fileName)
+    printDictionary()
+    graph = generateGraph(list(sentenceDictionary.keys()))
+    pageRank = networkx.pagerank(graph)
+    return sorted(pageRank, key=pageRank.get, reverse=True)[:summarySentenceCount]
+
+
+if __name__ == "__main__":
+    print(textRankSimilarity(5))
