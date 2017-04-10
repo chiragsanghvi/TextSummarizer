17fc598138c49c675ac2c1c548d2358c78ba3f0b
Chirag Sanghvi <csanghvi@appacitive.com>
2017-04-09 20:33:42 -0700

    Reran batch for marathi

--- summarizer/text_rank_marathi_batch.py
+++ summarizer/text_rank_marathi_batch.py
@@ -6,7 +6,7 @@ docsFolder = "../Marathi/documents/"
 for root, dirs, files in os.walk(docsFolder):
     for filename in files:
         print(filename)
-        os.system("python text_rank_positional.py " + (docsFolder + filename) + " 5 10")
+        os.system("python text_rank_positional.py " + (docsFolder + filename) + " 5 2")
         print("Positional Done")
         os.system("python text_rank_similarity.py " + (docsFolder + filename))
         print("Similarity Done")
