6ca1a5577bfd08f1a456c131731b36fbda8794a7
palenq <palenq@gmail.com>
2017-04-10 15:55:33 -0700

    Added document with Rouge output results

--- summarizer/rouge.py
+++ summarizer/rouge.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 
 # LANGUAGE = "Marathi"
@@ -11,53 +12,54 @@ def compute_bigram_from_file(file_path):
         text = f.read()
         return compute_bigrams([text])
 
-for language in ["Portuguese", "Marathi"]:
-    print language.upper()
-    print "------------"
+with open(os.path.join("..", "summarizer", "evaluation_results", "rouge.txt"), "w") as f_output:
+    for language in ["Portuguese", "Marathi"]:
+        print(language.upper(), file=f_output)
+        print("------------", file=f_output)
 
-    if language == "Portuguese":
-        human_summaries_folder = os.path.join("human_summaries")
-        system_summaries_folder = os.path.join("generated_summaries")
-        summarizer_types = ["LexRankSummarizer", "LsaSummarizer", "LuhnSummarizer", "SumBasicSummarizer", "TextRankSummarizer"]
+        if language == "Portuguese":
+            human_summaries_folder = os.path.join("human_summaries")
+            system_summaries_folder = os.path.join("generated_summaries")
+            summarizer_types = ["LexRankSummarizer", "LsaSummarizer", "LuhnSummarizer", "SumBasicSummarizer", "TextRankSummarizer"]
 
-    elif language == "Marathi":
-        human_summaries_folder = os.path.join("..", "Marathi", "summaries")
-        system_summaries_folder = os.path.join("..", "Marathi", "summaries")
-        summarizer_types = ["TextRankPositionalSummarizer", "TextRankSimilaritySummarizer"]
+        elif language == "Marathi":
+            human_summaries_folder = os.path.join("..", "Marathi", "summaries")
+            system_summaries_folder = os.path.join("..", "Marathi", "summaries")
+            summarizer_types = ["TextRankPositionalSummarizer", "TextRankSimilaritySummarizer"]
 
-    for type_summarizer in summarizer_types:
-        avg_rouge_2_score = 0.0
-        total_files = 0
+        for type_summarizer in summarizer_types:
+            avg_rouge_2_score = 0.0
+            total_files = 0
 
-        for file_reference in os.listdir(human_summaries_folder):
-            reference_summary_file_path = os.path.join(human_summaries_folder, file_reference)
-            reference_summary_bigrams = compute_bigram_from_file(reference_summary_file_path)
-            # print reference_summary_bigrams
+            for file_reference in os.listdir(human_summaries_folder):
+                reference_summary_file_path = os.path.join(human_summaries_folder, file_reference)
+                reference_summary_bigrams = compute_bigram_from_file(reference_summary_file_path)
+                # print reference_summary_bigrams
 
-            file_candidate = file_reference.replace("Human", type_summarizer)
-            candidate_summary_file_path = os.path.join(system_summaries_folder, file_candidate)
-            candidate_summary_bigrams = compute_bigram_from_file(candidate_summary_file_path)
+                file_candidate = file_reference.replace("Human", type_summarizer)
+                candidate_summary_file_path = os.path.join(system_summaries_folder, file_candidate)
+                candidate_summary_bigrams = compute_bigram_from_file(candidate_summary_file_path)
 
-            num_cooccurrences = 0
-            for elem in candidate_summary_bigrams:
-                if elem in reference_summary_bigrams:
-                    num_cooccurrences += 1
-            # print candidate_summary_bigrams
-            # print reference_summary_bigrams
+                num_cooccurrences = 0
+                for elem in candidate_summary_bigrams:
+                    if elem in reference_summary_bigrams:
+                        num_cooccurrences += 1
+                # print candidate_summary_bigrams
+                # print reference_summary_bigrams
 
-            # print num_cooccurrences
-            # print len(reference_summary_bigrams)
+                # print num_cooccurrences
+                # print len(reference_summary_bigrams)
 
-            rouge_2_score = float(num_cooccurrences) / float(len(reference_summary_bigrams))
-            # print rouge_2_score
+                rouge_2_score = float(num_cooccurrences) / float(len(reference_summary_bigrams))
+                # print rouge_2_score
 
-            avg_rouge_2_score += rouge_2_score
-            total_files += 1
+                avg_rouge_2_score += rouge_2_score
+                total_files += 1
 
-        avg_rouge_2_score = avg_rouge_2_score / float(total_files)
+            avg_rouge_2_score = avg_rouge_2_score / float(total_files)
 
-        print "Method: %s" %type_summarizer
-        print "Average ROUGE-2 score", avg_rouge_2_score
-        print
+            print("Method: %s" %type_summarizer, file=f_output)
+            print("Average ROUGE-2 score", avg_rouge_2_score, file=f_output)
+            print(file=f_output)
 
 
