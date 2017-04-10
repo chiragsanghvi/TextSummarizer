7f27070f1132fb71e4f030d0ee79da4b96829923
palenq <palenq@gmail.com>
2017-04-09 22:36:06 -0700

    Added Rouge evaluation algorithm

---
+++ summarizer/system_summaries/rouge.py
@@ -0,0 +1,58 @@
+import os
+
+# LANGUAGE = "Marathi"
+
+def compute_bigrams(text):
+    bigrams = [b for l in text for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
+    return bigrams
+
+def compute_bigram_from_file(file_path):
+    with open(file_path, "rb") as f:
+        text = f.read()
+        return compute_bigrams([text])
+
+for language in ["Portuguese", "Marathi"]:
+    if language == "Portuguese":
+        human_summaries_folder = os.path.join("..", "human_summaries")
+        system_summaries_folder = os.path.join("..", "generated_summaries")
+        type_summarizer = "TextRankSummarizer"
+
+    elif language == "Marathi":
+        human_summaries_folder = os.path.join("..", "..", "Marathi", "summaries")
+        system_summaries_folder = os.path.join("..", "..", "Marathi", "summaries")
+        type_summarizer = "TextRankPositionalSummarizer"
+
+    avg_rouge_2_score = 0.0
+    total_files = 0
+
+    for file_reference in os.listdir(human_summaries_folder):
+        reference_summary_file_path = os.path.join(human_summaries_folder, file_reference)
+        reference_summary_bigrams = compute_bigram_from_file(reference_summary_file_path)
+        # print reference_summary_bigrams
+
+        file_candidate = file_reference.replace("Human", type_summarizer)
+        candidate_summary_file_path = os.path.join(system_summaries_folder, file_candidate)
+        candidate_summary_bigrams = compute_bigram_from_file(candidate_summary_file_path)
+
+        num_cooccurrences = 0
+        for elem in candidate_summary_bigrams:
+            if elem in reference_summary_bigrams:
+                num_cooccurrences += 1
+        # print candidate_summary_bigrams
+        # print reference_summary_bigrams
+
+        # print num_cooccurrences
+        # print len(reference_summary_bigrams)
+
+        rouge_2_score = float(num_cooccurrences) / float(len(reference_summary_bigrams))
+        # print rouge_2_score
+
+        avg_rouge_2_score += rouge_2_score
+        total_files += 1
+
+    avg_rouge_2_score = avg_rouge_2_score / float(total_files)
+    print language
+    print "Average ROUGE-2 score", avg_rouge_2_score
+    print
+
+
