42fd4e56e00732eaa0c9c747f76f9c0c90c3650a
Chirag Sanghvi <csanghvi@appacitive.com>
2017-04-28 16:50:35 -0700

    Removed portuguese

--- summarizer/text_rank_portuguese_batch.py
+++ summarizer/text_rank_portuguese_batch.py
@@ -6,8 +6,8 @@ docsFolder = "../Portuguese/documents/"
 for root, dirs, files in os.walk(docsFolder):
     for filename in files:
         print(filename)
-        #os.system("python text_rank_positional_portuguese.py " + (docsFolder + filename) + " 5 2")
-        #print("Positional Done")
+        os.system("python text_rank_positional_portuguese.py " + (docsFolder + filename) + " 5 2")
+        print("Positional Done")
         os.system("python text_rank_similarity_portuguese.py " + (docsFolder + filename))
         print("Similarity Done")
 
